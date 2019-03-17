6239e658a0d6d64c906603e42fc5a2fbdd97a767
Temur <tim_1998@bk.ru>
2019-03-17 18:47:01 +0300

    fix: normalize an example's input data
    #fix #code

--- assignment.01/ex1_multi.m
+++ assignment.01/ex1_multi.m
@@ -104,7 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = [1 1650 3] * theta; % You should change this
+x = [1 featureNormalize([1650 3])];
+price = x * theta; % You should change this
 
 % ============================================================
 
